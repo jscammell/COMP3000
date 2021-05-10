54b95a4260587520ff56cbcaa15216f9ec0d1795
josh <josh@kali>
2021-05-10 15:47:31 +0100

    Nmap home screen made to split up functions correctly

--- src/Classes/NmapScanClass.java
+++ src/Classes/NmapScanClass.java
@@ -7,7 +7,7 @@ package Classes;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.ArrayList; // import the ArrayList class
+import java.util.ArrayList; 
 
 
 /**
